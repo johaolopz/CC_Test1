a6608507ac939ef455cc82259e5b09c2bc1fd974
Johao Lopez <johao.lopz.m@gmail.com>
2021-05-13 19:50:11 -0500

    uso de printf y salto de linea

--- HolaMundoCC.cc
+++ HolaMundoCC.cc
@@ -8,6 +8,10 @@ int main()
     int num = 2;
     string texto = "es el numero que multiplicado por si mismo, nos resulta: ";
     int num2 = num*num;
-    cout << num << " " << texto << num2 << endl;
+    cout << num << " " << texto << num2 << "\n" << endl;
+
+    //Ahora imprimir por pantalla con printf
+    printf("%d;%s%d", num, texto.c_str(), num2);
+
     return 0;
 }
